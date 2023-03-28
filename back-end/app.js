c8846360b63e0477d4018c9459802da3955ca0ab
mehdimez123 <m.meziane@esi-sba.dz>
2023-03-28 17:22:22 +0200

    changement

--- back-end/app.js
+++ back-end/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/../front-end/html/login.html`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
