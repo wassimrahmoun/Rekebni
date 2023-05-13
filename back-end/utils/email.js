06bc6a5f0bf38841ecc1be0de48aea45400b3081
BaghHadi <baghdadli.abdelhadi@gmail.com>
2023-05-13 23:21:04 +0100

    sending real emails

--- back-end/utils/email.js
+++ back-end/utils/email.js
@@ -1,27 +1,59 @@
 const nodemailer = require("nodemailer");
+// const pug = require("pug");
 
-const sendEmail = async (options) => {
-  // 1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+module.exports = class Email {
+  constructor(user) {
+    this.to = user.email;
+    this.firstName = user.name;
+    this.from = `WASSELNI <${process.env.EMAIL_FROM}>`;
+  }
 
-  //2 define the meail option
-  const mailOptions = {
-    from: "HADI BAGHDADLI <rekebni@hadi.io>",
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html:
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === "production") {
+      // Sendgrid
+      return nodemailer.createTransport({
+        service: "SendGrid",
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
+    }
 
-  //3 actiammu send the email
-  await transporter.sendMail(mailOptions);
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  // Send the actual email
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template
+
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      text: "Hello nous somme WASSELNI",
+    };
+
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send("welcome", "Welcome to the WASSELNI Family!");
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      "passwordReset",
+      "Your password reset token (valid for only 10 minutes)"
+    );
+  }
 };
-
-module.exports = sendEmail;
