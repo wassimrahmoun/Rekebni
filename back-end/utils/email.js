f297c3bcd0c1e7ab71c46876dcce2bad636aa43a
BaghHadi <baghdadli.abdelhadi@gmail.com>
2023-05-18 01:44:41 +0100

    version final mail welcom and forgot password with url

--- back-end/utils/email.js
+++ back-end/utils/email.js
@@ -1,4 +1,5 @@
 const nodemailer = require("nodemailer");
+const fs = require("fs");
 // const htmlToText = require("html-to-text");
 
 module.exports = class Email {
@@ -32,13 +33,15 @@ module.exports = class Email {
   }
 
   // Send the actual email
-  async send(subject, text) {
+  async send(subject, text, ht) {
+    const html = ht;
     // Define email options
     const mailOptions = {
       from: this.from,
       to: this.to,
       subject,
       text,
+      html,
     };
 
     // Create a transport and send email
@@ -47,13 +50,28 @@ module.exports = class Email {
 
   async sendWelcome() {
     const subject = "Welcome to the WASSELNI Family!";
+
     const text = `Dear ${this.firstName},\n\nWelcome to the WASSELNI Family! Please click on the following link to get started:\n${this.url}\n\nBest regards,\nWASSELNI Team`;
-    await this.send(subject, text);
+
+    const html = fs.readFileSync(
+      "back-end/public/html/welcomemail.html",
+      "utf8"
+    );
+    const modifiedHtml = html.replace("{{url}}", this.url);
+    //reste a le remplacer dans le html jcp comment faire
+    await this.send(subject, text, modifiedHtml);
   }
 
   async sendPasswordReset() {
-    const subject = "Your password reset token (valid for only 10 minutes)";
+    const subject = `Your password reset token (valid for only 10 minutes)`;
     const text = `Dear ${this.firstName},\n\nYou have requested to reset your password. Please click on the following link to reset it (valid for only 10 minutes):\n${this.url}\n\nIf you didn't request this, please ignore this email.\n\nBest regards,\nWASSELNI Team`;
-    await this.send(subject, text);
+
+    const html = fs.readFileSync(
+      "back-end/public/html/réinitialisermdp.html",
+      "utf8"
+    );
+    const modifiedHtml = html.replace("{{url}}", this.url);
+
+    await this.send(subject, text, modifiedHtml);
   }
 };
