05a9b738b2613cb948d131d5ec2dfc1401f92571
BaghHadi <100039164+HadiWax@users.noreply.github.com>
2023-05-19 14:10:46 +0100

    sfdd

--- back-end/public/js/addReview.js
+++ back-end/public/js/addReview.js
@@ -1,24 +1,20 @@
 var user = JSON.parse(window.localStorage.getItem("userJson"));
-var userId ;
-if (user) userId = user.id ;
+var userId;
+if (user) userId = user.id;
 
-
-
-
-document.addEventListener("DOMContentLoaded",async function(){
-    
-    const url = "http://localhost:8000/api/v1/reviews"
-    const conducteurId = 20 ;
-    const res = await fetch(url, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              review:"",
-              rating:"",
-              conducteur:"",
-              user:userId
-            }),
-          });
-})
+document.addEventListener("DOMContentLoaded", async function () {
+  const url = "http://localhost:8000/api/v1/reviews";
+  const conducteurId = 20;
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      review: "",
+      rating: "",
+      conducteur: "",
+      user: userId,
+    }),
+  });
+});
