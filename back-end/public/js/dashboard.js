32ca9dc707ae329c2e38e74be5d195d2bafb8481
XeedOus <djezirixeedoussama@gmail.com>
2023-05-19 20:45:31 +0100

    dont touch me (or else?)

--- back-end/public/js/dashboard.js
+++ back-end/public/js/dashboard.js
@@ -1,131 +1,136 @@
 var user = JSON.parse(window.localStorage.getItem("userJson"));
-var userId ;
-var userPic ; var userName ;  var userRating ; var userPseudo ; var userPhone ; var userEmail ;
-var token = window.localStorage.getItem("userToken") ;
-if(user)  {userId = user.id
-           userPic = user.photo ;
-           userName = user.name ;
-           userRating = user.ratingsAverage ;
-           userPseudo = user.pseudo ;
-           userPhone=user.phone ;
-           userPhoneFormatted = userPhone.slice(4);  // j'envoie le numero avec 213
-           userEmail = user.email ; } ;
-
- const signOutEventListener=function(){
-    const profilSignOut = document.querySelector(".disconnect"); // Déconnecter
-     profilSignOut.addEventListener("click", async function () {
-            await fetch("http://localhost:8000/api/v1/users/logout") ;
-            window.localStorage.removeItem("userJson") ;
-            window.location.href = "/";
-               
-             });
-         }          
-
-function afficherEtoiles(ranking) {
-    const fullStars = Math.floor(ranking);
-    const partialStar = Math.round((ranking - fullStars) * 10) / 10;
-    let html = "";
-    for (let i = 1; i <= fullStars; i++) {
-        html += '<img src="../img/star.png" class="head-rating-star" >';
-            }
-            if (partialStar >= 0.5) {
-              html += '<img src="../img/half-star.png" class="head-rating-star" >';
-            }
-            for (let i = Math.ceil(ranking)+1; i <= 5; i++) {
-              html += '<img src="../img/empty-star.png" class="head-rating-star" >';
-            }
-            return html;
-          }    
-
-const openCloseTrajetElementEventsListener = function(){
-    console.log(userId) ;
-        var trajetElements = document.querySelectorAll(".trajet");
-        var trajetBottomElements = document.querySelectorAll(".trajet-bottom");
-        var buttonElements = document.querySelectorAll("button");
-
-        for(let i=0 ; i<trajetElements.length;i++){ 
-        if(!trajetElements[i].classList.contains("past")){
-        trajetElements[i].addEventListener("click", function (event) {  
-            trajetBottomElements[i].classList.toggle("open");
-            trajetElements[i].classList.toggle("opened"); 
-        })}
-      
-    }
-
-        document.addEventListener("click", function (event) {
-          for(let i=0 ; i<trajetElements.length;i++){
-          if (
-            trajetBottomElements[i].classList.contains("open") &&
-            event.target !== buttonElements[i] &&
-            !trajetElements[i].contains(event.target)
-            
-          ) {
-            trajetBottomElements[i].classList.remove("open");
-            trajetElements[i].classList.remove("opened");
-          }
-       } });
-
-        buttonElements.forEach(btn=>{btn.addEventListener("click", function (event) {
-          event.stopPropagation();
-        })
-        });
-      
+var userId;
+var userPic;
+var userName;
+var userRating;
+var userPseudo;
+var userPhone;
+var userEmail;
+var token = window.localStorage.getItem("userToken");
+if (user) {
+  userId = user.id;
+  userPic = user.photo;
+  userName = user.name;
+  userRating = user.ratingsAverage;
+  userPseudo = user.pseudo;
+  userPhone = user.phone;
+  userPhoneFormatted = userPhone.slice(4); // j'envoie le numero avec 213
+  userEmail = user.email;
 }
 
+const signOutEventListener = function () {
+  const profilSignOut = document.querySelector(".disconnect"); // Déconnecter
+  profilSignOut.addEventListener("click", async function () {
+    await fetch("http://localhost:8000/api/v1/users/logout");
+    window.localStorage.removeItem("userJson");
+    window.location.href = "/";
+  });
+};
 
-document.addEventListener("DOMContentLoaded",async function(){
-    signOutEventListener() ;
-    
-    const profilTabInfos = function(){
-    const imgElement = document.querySelector(".head-pfp") ;
-    const ratingsStarsElement = document.querySelector(".head-rating-stars") ;
-    const ratingElement = document.querySelector(".head-rating-score") ;
-    const nameElement = document.querySelector(".head-name-fullname") ;
-    const pseudoElement = document.getElementById("pseudo") ;
+function afficherEtoiles(ranking) {
+  const fullStars = Math.floor(ranking);
+  const partialStar = Math.round((ranking - fullStars) * 10) / 10;
+  let html = "";
+  for (let i = 1; i <= fullStars; i++) {
+    html += '<img src="../img/star.png" class="head-rating-star" >';
+  }
+  if (partialStar >= 0.5) {
+    html += '<img src="../img/half-star.png" class="head-rating-star" >';
+  }
+  for (let i = Math.ceil(ranking) + 1; i <= 5; i++) {
+    html += '<img src="../img/empty-star.png" class="head-rating-star" >';
+  }
+  return html;
+}
+
+const openCloseTrajetElementEventsListener = function () {
+  console.log(userId);
+  var trajetElements = document.querySelectorAll(".trajet");
+  var trajetBottomElements = document.querySelectorAll(".trajet-bottom");
+  var buttonElements = document.querySelectorAll("button");
+
+  for (let i = 0; i < trajetElements.length; i++) {
+    if (!trajetElements[i].classList.contains("past")) {
+      trajetElements[i].addEventListener("click", function (event) {
+        trajetBottomElements[i].classList.toggle("open");
+        trajetElements[i].classList.toggle("opened");
+      });
+    }
+  }
+
+  document.addEventListener("click", function (event) {
+    for (let i = 0; i < trajetElements.length; i++) {
+      if (
+        trajetBottomElements[i].classList.contains("open") &&
+        event.target !== buttonElements[i] &&
+        !trajetElements[i].contains(event.target)
+      ) {
+        trajetBottomElements[i].classList.remove("open");
+        trajetElements[i].classList.remove("opened");
+      }
+    }
+  });
+
+  buttonElements.forEach((btn) => {
+    btn.addEventListener("click", function (event) {
+      event.stopPropagation();
+    });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", async function () {
+  signOutEventListener();
+
+  const profilTabInfos = function () {
+    const imgElement = document.querySelector(".head-pfp");
+    const ratingsStarsElement = document.querySelector(".head-rating-stars");
+    const ratingElement = document.querySelector(".head-rating-score");
+    const nameElement = document.querySelector(".head-name-fullname");
+    const pseudoElement = document.getElementById("pseudo");
     const phoneElement = document.getElementById("telephone");
     const emailElement = document.getElementById("email");
-    const html = afficherEtoiles(userRating) ;
+    const html = afficherEtoiles(userRating);
 
-    imgElement.src = `../img/user/${userPic}` ;
-    ratingsStarsElement.innerHTML = html ;
-    ratingElement.textContent = userRating ;
-    nameElement.textContent = userName ;
-    pseudoElement.textContent = userPseudo ;
-    phoneElement.textContent = userPhoneFormatted ;
-    emailElement.textContent = userEmail ;
-    }
+    imgElement.src = `../img/user/${userPic}`;
+    ratingsStarsElement.innerHTML = html;
+    ratingElement.textContent = userRating;
+    nameElement.textContent = userName;
+    pseudoElement.textContent = userPseudo;
+    phoneElement.textContent = userPhoneFormatted;
+    emailElement.textContent = userEmail;
+  };
 
-    profilTabInfos() ;
+  profilTabInfos();
 
-    const trajetsContainer = document.querySelector(".trajets") ;
-    const url = `http://localhost:8000/api/v1/trajets/conducteur/${userPseudo}` ;
-    const res = await fetch(url , {
-        method: "GET",
-        headers: {
-         "Content-Type": "application/json",
-         "Authorization": `Bearer ${token}` 
-        }
-      });
-    // console.log(res) ;
-    const trajets = (await res.json()).data.trajet ;  // array of current user trajets
-    // console.log(trajets) ;
-    
-    const mesTrajets = function(){
-        console.log(trajets);
-    trajets.forEach(trajet=>{
-    const date =  new Date(trajet.date) ;
-    const day = String(date.getDate()).padStart(2,"0") ;
-    const month = String(date.getMonth()).padStart(2,"0") ;
-    const year = date.getFullYear() 
-    const passagers = trajet.Passagers ;
-    var htmlP ="" ;
-    passagers.forEach(passager=>{
-        htmlP+=`<div class="passager-info">
+  const trajetsContainer = document.querySelector(".trajets");
+  const url = `http://localhost:8000/api/v1/trajets/conducteur/${userPseudo}`;
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  // console.log(res) ;
+  const trajets = (await res.json()).data.trajet; // array of current user trajets
+  // console.log(trajets) ;
+
+  const mesTrajets = function () {
+    console.log(trajets);
+    trajets.forEach((trajet) => {
+      const date = new Date(trajet.date);
+      const day = String(date.getDate()).padStart(2, "0");
+      const month = String(date.getMonth()).padStart(2, "0");
+      const year = date.getFullYear();
+      const passagers = trajet.Passagers;
+      var htmlP = "";
+      passagers.forEach((passager) => {
+        htmlP += `<div class="passager-info">
         <img class="passager-pfp" src="../img/user/${passager.photo}" />
         <span class="passager-name">${passager.name}}</span>
-      </div>`
-    })
-    const html = `<div class="trajet ${trajet.isActive?"":"past"} ">
+      </div>`;
+      });
+      const html = `<div class="trajet ${trajet.isActive ? "" : "past"} ">
     <div class="trajet-top">
       <div class="left">
         <div class="trip-info">
@@ -184,37 +189,40 @@ document.addEventListener("DOMContentLoaded",async function(){
       ${htmlP}
     </div>
     <button class="cancel-trip" id="${trajet.id}">annuler le trajet</button>  
-  </div>`
+  </div>`;
 
-  trajetsContainer.insertAdjacentHTML("beforeend",html) ;
-    }) ;
-    openCloseTrajetElementEventsListener() ;
+      trajetsContainer.insertAdjacentHTML("beforeend", html);
+    });
+    openCloseTrajetElementEventsListener();
+  };
 
-    
-     }
+  mesTrajets();
 
-     mesTrajets() ;
-
-     const mesReservations =async function(){
-        // const res = await fetch(`http://localhost:8000/api/v1/trajets/passager/645e8fd0ddef963735c6e2c9`,{  // mettre userId aprés
-        const res = await fetch(`http://localhost:8000/api/v1/trajets/passager/${userId}`,{
-         method: "GET",
+  const mesReservations = async function () {
+    // const res = await fetch(`http://localhost:8000/api/v1/trajets/passager/645e8fd0ddef963735c6e2c9`,{  // mettre userId aprés
+    const res = await fetch(
+      `http://localhost:8000/api/v1/trajets/passager/${userId}`,
+      {
+        method: "GET",
         headers: {
-         "Content-Type": "application/json",
-         "Authorization": `Bearer ${token}` 
-        }
-        })
-        console.log(res) ;
-        const reservations = (await res.json()).data.trajet ;
-        const reservationsContainer = document.querySelector(".reservations") ;
-        reservations.forEach(reservation=>{
-          console.log(reservation);
-           if(reservation.Conducteur){
-           const date =  new Date(reservation.date) ;
-           const day = String(date.getDate()).padStart(2,"0") ;
-           const month = String(date.getMonth()).padStart(2,"0") ;
-           const year = date.getFullYear() 
-           const html =` <div class="result ${reservation.isActive?"":"past"} "}>
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    console.log(res);
+    const reservations = (await res.json()).data.trajet;
+    const reservationsContainer = document.querySelector(".reservations");
+    reservations.forEach((reservation) => {
+      console.log(reservation);
+      if (reservation.Conducteur) {
+        const date = new Date(reservation.date);
+        const day = String(date.getDate()).padStart(2, "0");
+        const month = String(date.getMonth()).padStart(2, "0");
+        const year = date.getFullYear();
+        const html = ` <div class="result ${
+          reservation.isActive ? "" : "past"
+        } "}>
            <div class="left">
              <div class="driver-info">
                <div>
@@ -289,7 +297,9 @@ document.addEventListener("DOMContentLoaded",async function(){
                <span class="price-value white">${reservation.Prix}</span>
                <span class="price-text white">DA</span>
              </div>
-             ${reservation.isActive?` <div class="remaining-places white">
+             ${
+               reservation.isActive
+                 ? ` <div class="remaining-places white">
                                        <span class="remaining-places-text white">Places restantes:</span>
                                        <ion-icon
                                               name="person-outline"
@@ -299,61 +309,133 @@ document.addEventListener("DOMContentLoaded",async function(){
                                         ></ion-icon>
                                             <span class="remaining-places-value white">${reservation.places}</span>
                                             </div>`
-            :  `                        <a href="/html/addReview.html">
+                 : `                        <a href="/html/addReview.html">
                                            <div class="leave-a-review" id=${reservation.Conducteur.id}>
                                             <span>laisser un avis</span>
                                            </div>
-                                         </a>`}
+                                         </a>`
+             }
         
            </div>
-         </div>`  ;
-         reservationsContainer.insertAdjacentHTML("afterbegin",html) ;
-           }
-         
-        })
+         </div>`;
+        reservationsContainer.insertAdjacentHTML("afterbegin", html);
+      }
+    });
 
-        const reviewEventListener = function(){
-          const reviewBtns = document.querySelectorAll(".leave-a-review") ;
-          reviewBtns.forEach(reviewBtn=>{reviewBtn.addEventListener("click",function(){
-            const conducteurId = this.id ;
-            window.localStorage.setItem("conducteurId",conducteurId) ;
-          })}) ;
-         }
-    
-         reviewEventListener() ;
-    
+    const reviewEventListener = function () {
+      const reviewBtns = document.querySelectorAll(".leave-a-review");
+      reviewBtns.forEach((reviewBtn) => {
+        reviewBtn.addEventListener("click", function () {
+          const conducteurId = this.id;
+          window.localStorage.setItem("conducteurId", conducteurId);
+        });
+      });
+    };
 
-     }
+    reviewEventListener();
+  };
 
-     
-     mesReservations() ;
-  
-     const deleteTrajetEventListener = function(){
-        const deleteTrajetElement = document.querySelectorAll(".cancel-trip") ;
-        deleteTrajetElement.forEach(elmnt=>{elmnt.addEventListener("click",async function(){
-          const trajetId = this.id ;
-          const res = await fetch(`http://localhost:8000/api/v1/trajets/${trajetId}`,{
-            method:"DELETE"
-          })
-          if (res.ok) {mesTrajets() ;
-                      window.location.href="/html/dashboard.html" ;
+  mesReservations();
+
+  const deleteTrajetEventListener = function () {
+    const deleteTrajetElement = document.querySelectorAll(".cancel-trip");
+    deleteTrajetElement.forEach((elmnt) => {
+      elmnt.addEventListener("click", async function () {
+        const trajetId = this.id;
+        const res = await fetch(
+          `http://localhost:8000/api/v1/trajets/${trajetId}`,
+          {
+            method: "DELETE",
           }
-        })}) 
-     }
+        );
+        if (res.ok) {
+          mesTrajets();
+          window.location.href = "/html/dashboard.html";
+        }
+      });
+    });
+  };
 
-     deleteTrajetEventListener() ;
+  deleteTrajetEventListener();
 
-     const deleteAccountEventListener = function(){
-       const deleteAccountElement = document.querySelector(".delete-account") ;
-       deleteAccountElement.addEventListener("click",async function(){
-        const url =`http://localhost:8000/api/v1/users/${userId}` ;
-        const res = await fetch(url,{
-            method:"DELETE"
-        })
-        if(res.ok) { window.localStorage.removeItem("userJson");
-                     window.location.href="/" ;}
-       })
-     }
-     
-     deleteAccountEventListener() ;
-}) ;
+  const deleteAccountEventListener = function () {
+    const deleteAccountElement = document.querySelector(".delete-account");
+    deleteAccountElement.addEventListener("click", async function () {
+      const url = `http://localhost:8000/api/v1/users/${userId}`;
+      const res = await fetch(url, {
+        method: "DELETE",
+      });
+      if (res.ok) {
+        window.localStorage.removeItem("userJson");
+        window.location.href = "/";
+      }
+    });
+  };
+
+  deleteAccountEventListener();
+});
+
+////////////////////////////////////////////////////////////////////////////////////
+
+document.addEventListener("DOMContentLoaded", function () {
+  var trajetElements = document.querySelectorAll(".trajet");
+  var trajetBottomElements = document.querySelectorAll(".trajet-bottom");
+  var buttonElements = document.querySelectorAll("button");
+
+  for (let i = 0; i < trajetElements.length; i++) {
+    if (!trajetElements[i].classList.contains("past")) {
+      trajetElements[i].addEventListener("click", function (event) {
+        trajetBottomElements[i].classList.toggle("open");
+        trajetElements[i].classList.toggle("opened");
+      });
+    }
+  }
+
+  document.addEventListener("click", function (event) {
+    for (let i = 0; i < trajetElements.length; i++) {
+      if (
+        trajetBottomElements[i].classList.contains("open") &&
+        event.target !== buttonElements[i] &&
+        !trajetElements[i].contains(event.target)
+      ) {
+        trajetBottomElements[i].classList.remove("open");
+        trajetElements[i].classList.remove("opened");
+      }
+    }
+  });
+
+  buttonElements.forEach((btn) => {
+    btn.addEventListener("click", function (event) {
+      event.stopPropagation();
+    });
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  var cancelButtonElements = document.querySelectorAll(".edit--content");
+  var textElements = document.querySelectorAll(".button-text");
+  var inputParents = document.querySelectorAll(".input-parent");
+  var savePopUp = document.querySelector(".save-alert");
+  var b = false;
+  // var buttonElements = document.querySelectorAll(".button");
+  for (let i = 0; i < cancelButtonElements.length; i++) {
+    if (!cancelButtonElements[i].classList.contains("special")) {
+      cancelButtonElements[i].addEventListener("click", function (event) {
+        textElements[i].classList.toggle("redify");
+        cancelButtonElements[i].classList.toggle("redifyed");
+        inputParents[i].classList.toggle("unshown");
+      });
+    }
+  }
+
+  // document.addEventListener("click", function (event) {
+  //   for (let i = 0; i < cancelButtonElements.length; i++) {
+  //     if (
+  //       textElements[i].classList.contains("redify") &&
+  //       !cancelButtonElements[i].contains(event.target)
+  //     ) {
+  //       textElements[i].classList.remove("redify");
+  //       cancelButtonElements[i].classList.remove("redifyed");
+  //     }
+  //   }
+  // });
+});
