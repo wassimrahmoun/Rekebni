71a614c1844a83fa45393f523b6f1c2a490869e9
SenhadjiMSaid <saidsenhadji06@gmail.com>
2023-05-20 15:20:56 +0100

    ....

--- back-end/routes/trajetRoutes.js
+++ back-end/routes/trajetRoutes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 //   trajetController.getUserTrajects
 // );
 
-<<<<<<< HEAD
+
 router.get("/search", trajetController.searchTrajets); //je pense c mieuc dutiliser li deja rak khademha get zll + des filtres li mhedi ya"tehom f url
 
 router.get(
@@ -18,24 +18,20 @@ router.get(
   authController.isLoggedIn,
   authController.getCurrentUser
 );
-<<<<<<< HEAD
-=======
 
->>>>>>> said
-=======
->>>>>>> 55b9ef65b610f4f5d2732f282fa7c5d03e41f1f4
+
+
+
+
 router
   .route("/")
   .get(trajetController.getAllTrajets)
   .post(authController.isLoggedIn, trajetController.createTrajet);
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
 // router.route("/:slug/:date/:heurD").get(trajetController.getTrajet);
 router.route("/:id").get(trajetController.getTrajet);
-=======
->>>>>>> said
-=======
+
 
 // router.route("/:slug/:date/:heurD").get(trajetController.getTrajet);
 router
@@ -43,7 +39,7 @@ router
   .get(trajetController.getTrajet)
   .patch(trajetController.updateTrajet)
   .delete(trajetController.deleteTrajet);
->>>>>>> 55b9ef65b610f4f5d2732f282fa7c5d03e41f1f4
+
 
 router.use(authController.protect);
 router.route("/conducteur/:slug").get(trajetController.getUserTrajects);
